18b64a814f5004550991748d83495a98d5cd59ab
jonisb <github.com@JsBComputing.se>
2024-09-02 10:34:30 +0200

    Save pairing key

--- weboscli.py
+++ weboscli.py
@@ -12,9 +12,14 @@ def main():
 
 
 def save_settings(settings):
+    settings_no_key = settings.copy()
+    client_key = settings_no_key.pop("client_key")
+    if keyring.get_password(settings["service_id"], "client_key") != client_key:
+        keyring.set_password(service_id, "client_key", client_key)
+
     # save settings to settings.json
     with open("settings.json", "w") as f:
-        json.dump(settings, f)
+        json.dump(settings_no_key, f)
 
 
 def load_settings():
